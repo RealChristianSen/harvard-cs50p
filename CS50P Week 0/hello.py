1afb87aba5007c00ae6ba8d4b8209808c23c022b
Christiansen Paulson <133834581+RealChristianSen@users.noreply.github.com>
2026-03-24 15:48:56 -0700

    Initial upload of CS50P work

---
+++ CS50P Week 0/hello.py
@@ -0,0 +1,26 @@
+print("Hello, World!")
+
+# ask user for their name
+name = input("What is your name? ")
+
+#  whitespace from string
+name = name.strip()
+
+# capitalize user's name
+name = name.capitalize() 
+
+# capitalize first letter of each word in user's name
+name = name.title()
+
+# can chain methods together
+name = name.strip().title()
+
+# split user's name into first and last name
+first, last = name.split(" ")
+
+# print their name 
+print("Hello, " + name)
+print("Hello,", name)
+print(f"Hello, {name}")
+print("Hello,", name, sep = "_", end = "!\n" )
+print(f"Hello, {first}") 

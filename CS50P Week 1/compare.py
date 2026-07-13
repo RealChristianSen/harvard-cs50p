d22a6646331e2125bf42b266be0167c078995081
Christiansen Paulson <133834581+RealChristianSen@users.noreply.github.com>
2026-07-13 14:01:25 -0700

    finished compare.py program from CS50P week 1 lecture, conditionals

--- CS50P Week 1/compare.py
+++ CS50P Week 1/compare.py
@@ -1,8 +1,43 @@
-x = int(input("what's x? "))
-y = int(input("what's y? "))
 
+# only using if statements
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
 if x == y:
     print("x is equal to y")
 
+# using elif statements
+a = int(input("What's a? "))
+b = int(input("What's b? "))
+
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equal to b")
+    
+# using else statements
+c = int(input("What's c? "))
+d = int(input("What's d? "))
+
+if c < d:
+    print("c is less than d")
+elif c > d:
+    print("c is greater than d")
 else:
-    print("x is not equal to y")  
+    print("c is equal to d")
+
+# using or statement
+e = int(input("What's e? "))
+f = int(input("What's f? "))
+
+# can also use != or == in this scenario instead of an or statement
+if e < f or e > f:
+    print("e is not equal to f")
+else:
+    print("e is equal to f")

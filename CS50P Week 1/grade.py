0c24a3f697fc9194a03610cac658da76fbb9ebb1
Christiansen Paulson <133834581+RealChristianSen@users.noreply.github.com>
2026-07-13 14:19:30 -0700

    finished grade.py program of CS50P week 1, conditionals

--- CS50P Week 1/grade.py
+++ CS50P Week 1/grade.py
@@ -1,14 +1,14 @@
-score = int(input("Score:  "))
+score = int(input("Score: "))
 
 if score >= 90:
     print("Grade: A")
-    
+
 elif score >= 80:
-    print("Grade: B")
-    
-elif score >= 70   :
+    print("Grae: B")
+
+elif score >= 70:
     print("Grade: C")
-    
+
 elif score >= 60:
     print("Grade: D")
     

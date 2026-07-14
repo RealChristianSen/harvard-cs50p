f1db0b1f24cc3128198952060bb83ad5df96097f
Christiansen Paulson <133834581+RealChristianSen@users.noreply.github.com>
2026-07-14 13:00:42 -0700

    finished recommendations.py from CS50P week 1 short boolean expressions

--- CS50P Week 1/recommendations.py
+++ CS50P Week 1/recommendations.py
@@ -1,19 +1,25 @@
 def main():
     difficulty = input("Difficult or Casual? ")
+    if not(difficulty == "Difficult" or difficulty == "Casual"):
+        print("Please enter a valid difficulty")
+        return
+
     players = input("Multiplayer of Single-player? ")
+    if not (difficulty == "Mutliplayer" or difficulty == "Single-player"):
+        print("Please enter a valid number of players")
+        return
     
-    if difficulty == "Difficult":
-        if players == "Multiplayer":
-            recommend("Poker")
-        if players == "Single-player":
-            recommend("Klondike")
+    if difficulty == "Difficult" and players == "Multiplayer":
+        recommend("Poker")
+        
+    if difficulty == "Difficult" and players == "Single-player":
+        recommend("Klondike")
             
-    if difficulty == "Casual":
-        if players == "Multiplayer":
-            recommend("Hearts")
-        if players == "Single-player":
-            recommend("Clock")
-            
+    if difficulty == "Casual" and players == "Multiplayer":
+        recommend("Hearts")
+        
+    if difficulty == "Casual" and players == "Single-player":
+        recommend("Clock")
     
 def recommend(game):
     print("You might like", game)

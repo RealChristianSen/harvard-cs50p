11872747f900ca7f040c2a9ffcc5c77a05211a46
Christiansen Paulson <133834581+RealChristianSen@users.noreply.github.com>
2026-07-23 16:15:41 -0700

    finished search.py using artists.py and artwork.py custom modules of CS50P Week 4 Creating Modules and Packages Short

---
+++ CS50P Week 4 Libraries/search.py
@@ -0,0 +1,12 @@
+from artwork import get_artworks
+from artists import get_artists
+
+def main():
+    artist = input("Artist: ")
+    artists = get_artists(query=artist, limit=3)
+    
+    for artist in artists:
+        print(f"* {artist}")
+
+if __name__=="__main__":
+    main()

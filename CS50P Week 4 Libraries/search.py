4488ac4dadf4aac4ed9b4db971f49144676ab49f
Christiansen Paulson <133834581+RealChristianSen@users.noreply.github.com>
2026-07-23 16:50:47 -0700

    used black on search.py

--- CS50P Week 4 Libraries/search.py
+++ CS50P Week 4 Libraries/search.py
@@ -1,11 +1,13 @@
 from museum.artists import get_artists
 
+
 def main():
     artist = input("Artist: ")
     artists = get_artists(query=artist, limit=3)
-    
+
     for artist in artists:
         print(f"* {artist}")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
